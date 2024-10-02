d05c50bc224cba96206a980c394551d142e4ca20
buenojoao <joao220904@gmail.com>
2024-10-02 23:06:28 +0000

    aula 5

---
+++ aulas/aula05/operadores_logicos.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main (void) {
+
+  // E logico
+  int p = 0;
+  int q = 0;
+  printf("%i E %i = %i\n", p, q, p && q);
+  q = 1;
+  printf("%i E %i = %i\n", p, q, p && q);
+  q = 1;
+  p = 0;
+  printf("%i E %i = %i\n", p, q, p && q);
+  q = 1;
+  printf("%i E %i = %i\n", p, q, p && q);
+
+  //1<x>10
+  //1< X && X <10
+  
+  // OU logico
+  q = 0;
+  p = 0;
+  printf("%i OU %i = %i\n", p, q, p || q);
+  q = 1;
+  printf("%i OU %i = %i\n", p, q, p || q);
+  p = 1;
+  q = 0;
+  printf("%i OU %i = %i\n", p, q, p || q);
+  q = 1;
+  printf("%i OU %i = %i\n", p, q, p || q);
+
+  
+  // NAO logico
+  p = 0;
+  printf("NAO %i = %i\n", p, !p);
+  printf("NAO %i = %i\n", q, !q);
+
+  
+  return 0;
+}
---
+++ praticas/pratica05/operadores_aritmeticos.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main (void){
+
+  int numero1 = 0;
+  int numero2 = 0;
+  float numero3 = 0.0f;
+
+  printf("Digite um número inteiro: ");
+  scanf("%i", &numero1);
+  printf("Digite outro número inteiro: ");
+  scanf("%i", &numero2);
+  printf("Digite um número flutuante: ");
+  scanf("%f", &numero3);
+
+  int soma = numero1 + numero2;
+
+  printf("A soma de %i com %i é igual a %i\n", numero1, numero2, soma);
+
+  int subtracao = numero1 - numero2;
+
+  printf("A subtração de %i com %i é igual a %i\n", numero1, numero2, subtracao);
+
+  int multiplicacao = numero1 * numero2;
+
+  printf("A multiplicação de %i com %i é igual a %i\n", numero1, numero2, multiplicacao);
+
+  int divisao = numero1 / numero2;
+
+  printf ("A divisão de %i com %i é igual a %i\n", numero1, numero2, divisao);
+  
+  int resto_divisao = numero1 % numero2;
+
+  printf("O resto da divisão de %i por %i é igual %i\n", numero1,numero2, resto_divisao);
+  
+  float divisao_fracionada = numero1 / numero3;
+
+  printf("A divisão fracionada de %i por %.1f é igual a %.2f\n", numero1, numero3, divisao_fracionada);
+  
+  
+  return 0;
+}
