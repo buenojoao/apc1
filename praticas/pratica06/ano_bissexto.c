7d5685fe70d99b136f1e49f4dffabc50635e5eec
joao <joao220904@gmail.com>
2024-10-16 20:41:25 -0300

    praticas aula 06

--- praticas/pratica06/ano_bissexto.c
+++ praticas/pratica06/ano_bissexto.c
@@ -5,10 +5,14 @@ int main (void) {
     int ano = 0;
 
     printf("Digite um ano: ");
-    scanf("%i", &ano);
+    int deu_certo = scanf("%i", &ano);
     
-    int ano_eh_bissexto = ano % 4 || ano % 400 == 0;
-    
+    int ano_eh_bissexto = ano % 4 == 0 || ano % 400 == 0;
+    if (ano_eh_bissexto) {
+        printf("O ano eh bissexto!");
+    } else {
+        printf("O ano nao eh bissexto!");
+    }
     
     return 0;
 }
