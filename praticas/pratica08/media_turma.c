bb98dbf7e1686df75688be6ad644f6970d77405e
João Victor Bueno Almeida <joao220904@gmail.com>
2024-11-06 21:39:59 -0300

    aula 8+9

---
+++ praticas/pratica08/media_turma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void) {
+    float notas [10] = 0.0f;
+    float soma = 0.0f;
+    float media = 0.0f;
+    int qtd_acima_media;
+
+    for (int i=0; i<10; i++) {
+        scanf("%f", &notas[i]);
+    }
+
+
+    return 0;
+}
